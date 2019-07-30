09a1160fe2fc97effbcf5ef27bed007fe9b578cd
Sanath Kumar M S <sanathkumarms@Admins-Mac-mini-3.local>
2019-07-30 16:11:57 +0530

    First commit

---
+++ DemoProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Sanath Kumar M S on 30/07/19.
+//  Copyright © 2019 YML. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        var name = "Sanath Kumar"
+        print(name)
+    }
+
+
+}
+
